2e4570e243a4b9aedab13e8ac22f535dbd087457
Vinod Sabastian <vinod.sabastian@gmail.com>
2016-03-15 11:48:47 +0400

    fixed navigation errors

--- src/main/webapp/resources/theme1/js/property/app.js
+++ src/main/webapp/resources/theme1/js/property/app.js
@@ -14,22 +14,21 @@ var app = angular.module('propertyApp', ['ngSanitize','ngResource','ngRoute'])
 	.otherwise({
         redirectTo: '/list'
     });
-	
 }])
+
 .factory('PropertyService', function($resource) {
 	return $resource('json/property/:id');
 })
+
 .factory('PropertyListService', function($resource) {
 	return $resource('json/property/list');
 })
+
 .factory('SimilarPropertyService', function($resource) {
 	return $resource('json/property/similar/:id');
 });
 
-app.controller('PropertyController', function($scope, $route, $routeParams, $location, $log, $sce, PropertyService, SimilarPropertyService){
-	
-	$log.debug('$routeParams.id'+ $routeParams.id);
-	
+app.controller('PropertyController', function($scope, $route, $routeParams, $log, $sce, PropertyService, SimilarPropertyService){
 	PropertyService.get({id:$routeParams.id}, function(property) {
 		$scope.property = property;
 		$scope.selectedImage = angular.copy(property.imageList[0]);
@@ -43,14 +42,10 @@ app.controller('PropertyController', function($scope, $route, $routeParams, $loc
 	$scope.changeImage = function(image) {
 		$scope.selectedImage = image;
 	};
-	
-	$scope.go = function(path){
-	}
 });
 
 app.controller('ListController', function($scope, $log, PropertyListService) {
-	 PropertyListService.query(function(propertyList){
-		 $scope.propertyList = propertyList;
-		 $log.debug(propertyList);
-	 });
+	PropertyListService.query(function(propertyList){
+		$scope.propertyList = propertyList;
+	});
 });
